10f5f45b98314eec5123b388a9103d2799467a4c
Nikita <baynik01@gmail.com>
2022-06-14 11:10:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+  cw1.addEventListener("click", function () {
+    var n="";
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        for (var i = 0; i < array.length; i++){
+          n += JSON.stringify(array[i].userId)+"</br>"+JSON.stringify(array[i].id)+"      
+</br>"+JSON.stringify(array[i].title)+"</br>"+JSON.stringify(array[i].body)+"<hr>" ;
+        }
+        answer.innerHTML = n;
+      })
+  })
+
+  cw2.addEventListener("click", function () {
+    //TODO implement it
+  })
+
+  cw3.addEventListener("click", function () {
+    //TODO implement it
+  })
+
+})();
